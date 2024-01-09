add44d20c6c3da77c4b52345539c2e295553ce7c
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2024-01-09 11:02:34 +0200

    code

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -2,45 +2,6 @@
   <!-- component -->
   <main class="relative min-h-screen w-full bg-white">
     <div class="p-6" x-data="app">
-      <!-- header -->
-      <header class="flex w-full justify-between">
-        <svg
-          class="h-7 w-7 cursor-pointer text-gray-400 hover:text-gray-300"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-          stroke="currentColor"
-        >
-          <path
-            stroke-width="1"
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-
-        <!-- buttons -->
-        <div>
-          <button
-            type="button"
-            @click="isLoginPage = false"
-            v-show="isLoginPage"
-            class="rounded-2xl border-b-2 border-b-gray-300 bg-white px-4 py-3 font-bold text-blue-500 ring-2 ring-gray-300 hover:bg-gray-200 active:translate-y-[0.125rem] active:border-b-gray-200"
-          >
-            LOGIN
-          </button>
-
-          <button
-            type="button"
-            @click="isLoginPage = true"
-            v-show="!isLoginPage"
-            class="rounded-2xl border-b-2 border-b-gray-300 bg-white px-4 py-3 font-bold text-blue-500 ring-2 ring-gray-300 hover:bg-gray-200 active:translate-y-[0.125rem] active:border-b-gray-200"
-          >
-            SIGN UP
-          </button>
-        </div>
-      </header>
-
       <div
         class="absolute left-1/2 top-1/2 mx-auto max-w-sm -translate-x-1/2 -translate-y-1/2 transform space-y-4 text-center"
       >
@@ -114,7 +75,12 @@
               placeholder="Password"
               class="my-3 w-full border-none bg-transparent outline-none"
             />
-            <a href="#" class="font-medium text-gray-400 hover:text-gray-500">FORGOT?</a>
+            <a
+              @click="errorLogin = 'Too bad 💩'"
+              href="#"
+              class="font-medium text-gray-400 hover:text-gray-500"
+              >FORGOT?</a
+            >
           </div>
           <p v-show="errorLogin" class="text-sm text-red-700 italic">{{ errorLogin }}</p>
           <button
@@ -123,6 +89,19 @@
           >
             LOG IN
           </button>
+        </div>
+        <div>
+          <p v-show="isLoginPage">
+            Don't have an account?
+            <a href="#" class="text-blue-500" @click="isLoginPage = false"> LOGIN </a>
+            instead!
+          </p>
+
+          <p v-show="!isLoginPage">
+            Don't have an account?
+            <a href="#" class="text-blue-500" @click="isLoginPage = true"> SIGN UP </a>
+            instead!
+          </p>
         </div>
       </div>
     </div>
