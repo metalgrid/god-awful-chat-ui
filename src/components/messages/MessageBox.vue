f730a0d5025d6c681fba3014444a8146aef507e8
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2024-01-14 12:30:59 +0200

    image handling in the messagebox (paste + drag/drop)

--- src/components/messages/MessageBox.vue
+++ src/components/messages/MessageBox.vue
@@ -13,10 +13,22 @@
       <input
         v-model="text"
         @keyup.enter="sendMessage"
+        @paste="handlePaste"
+        @drop="handleDrop"
         type="text"
         class="w-full border-2 border-gray-100 rounded-xl px-4 py-1 outline-none text-gray-500 focus:outline-none focus:ring"
         placeholder="Write a message..."
       />
+      <div v-if="media" class="grid grid-cols-4 gap-2 mt-2">
+        <div class="relative" v-for="item in media" :key="item.id">
+          <fa-icon class="absolute bottom-0 right-0 w-4 h-4 bg-gray-50 rounded-full translate-x-2 translate-y-2" @click.prevent="media.splice(media.indexOf(item), 1)" :icon="['fas', 'times']" />
+          <img
+            v-if="item.type === 'image'"
+            :src="item.content"
+            class="rounded-md w-48"
+          />
+        </div>
+      </div>
     </div>
     <div class="flex flex-row">
       <button
@@ -43,6 +55,14 @@ const auth = unref(inject<Auth>('auth'))
 const convo = inject<Ref<Conversation & User>>('activeConvo')
 const text = ref('')
 
+type Media = {
+  id: number
+  type: 'image' | 'video' | 'file'
+  content: string | undefined
+}
+
+const media = ref<Media[]>([])
+
 const emit = defineEmits<{
   message: [MessageRequest]
 }>()
@@ -60,4 +80,41 @@ const sendMessage = () => {
   })
   text.value = ''
 }
+
+const handleDrop = (ev: DragEvent) => {
+  ev.preventDefault()
+  const files = ev.dataTransfer?.files || []
+  for (const file of files) {
+    if (file.type.indexOf('image') === -1) continue
+    const reader = new FileReader()
+    reader.onload = function () {
+      media.value.push({id: media.value.length, content: reader.result?.toString(), type: 'image'})
+    }
+    reader.readAsDataURL(file)
+  }
+}
+
+const handlePaste = (ev: ClipboardEvent) => {
+  const items = ev.clipboardData?.items || []
+
+  for (const item of items) {
+    const type = item.type;
+    if (type.indexOf("image") !== -1) {
+      const blob = item.getAsFile();
+      const reader = new FileReader();
+      reader.onload = function () {
+        media.value.push({id: media.value.length, content: reader.result?.toString(), type: 'image'});
+        // messages.value.push({
+        //   id: messages.value.length + 1,
+        //   text: img,
+        //   type: "image",
+        //   sender: user.id,
+        // });
+      };
+      if (blob) {
+        reader.readAsDataURL(blob);
+      }
+    }
+  }
+};
 </script>
