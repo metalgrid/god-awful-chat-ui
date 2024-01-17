e8dd58c22b873f27b94da759fdbf13f706ff9cf5
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2024-01-17 14:04:16 +0200

    calls, alpha 1 (elements in API, needs cleaning)

--- src/composables/call.ts
+++ src/composables/call.ts
@@ -13,6 +13,8 @@ export type Answer = {
   callee: User
 }
 
+type CallEventProps = {}
+
 export enum CallEvent {
   INCOMING_CALL = 'invite',
   ACCEPTED = 'accepted',
@@ -25,8 +27,9 @@ export enum CallEvent {
 
 export interface CallAPI {
   on: (event: CallEvent, callback: Function) => void
+  internal: (event: CallEvent, callback: Function) => void
   call: (username: string) => void
-  accept: (invite: Invite) => void
+  accept: (invite: Invite) => Promise<void>
 }
 
 const callbacks: Map<string, Function[]> = new Map()
@@ -51,6 +54,7 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
     }
 
     const api = {
+      internal: (event: CallEvent, callback: Function) => {},
       on: (event: CallEvent, callback: Function) => {
         console.log(`Registering callback for ${event}`)
 
@@ -72,12 +76,37 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
           throw new Error('STOMP communication socket is not connected!')
         }
 
+        const localICECandidates: RTCIceCandidate[] = []
         const localUUID = crypto.randomUUID()
+        let remoteUUID = ''
         console.log('Initiating Call UUID', localUUID, 'to', username)
 
-        const localICECandidates: RTCIceCandidate[] = []
         const pc = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
         pc.onsignalingstatechange = () => {
+          const sendICE = (event: RTCPeerConnectionIceEvent) => {
+            if (event.candidate) {
+              stompClient.send(
+                `/topic/calls/${remoteUUID}`,
+                { 'x-call-event': CallEvent.ICE_CANDIDATE },
+                JSON.stringify(event.candidate.toJSON())
+              )
+            }
+          }
+          if (pc.signalingState === 'stable') {
+            if (!remoteUUID) {
+              throw new Error('Remote UUID is not set, bur Peer connection is stable???')
+            }
+            console.log('[caller] Sending ICE candidates', localICECandidates)
+            localICECandidates.forEach((candidate) => {
+              stompClient.send(
+                `/topic/calls/${remoteUUID}`,
+                { 'x-call-event': CallEvent.ICE_CANDIDATE },
+                JSON.stringify(candidate.toJSON())
+              )
+            })
+            // register a handler for future ICE candidates
+            pc.onicecandidate = sendICE
+          }
           // exchange ICE candidates
           console.log('[caller] RTC Signaling state changed to:', pc.signalingState)
         }
@@ -88,12 +117,30 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
           console.log('[caller] RTC Connection state changed to:', pc.connectionState)
         }
 
-        pc.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
-          console.log('[caller] Got local ICE candidate', event.candidate)
+        pc.onicecandidate = (event) => {
+          if (event.candidate) {
+            localICECandidates.push(event.candidate)
+          }
+          // the caller may get their ICE candidates before the callee is ready to receive them, so store and ship on stable state
+        }
+
+        pc.ontrack = (event: RTCTrackEvent) => {
+          console.log('Got remote track', event)
+          const video = document.querySelector('#remote-video') as HTMLVideoElement
+          video.srcObject = event.streams[0]
+          video.onloadedmetadata = () => {
+            video.play()
+          }
         }
 
         const streams = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-        streams.getTracks().forEach((track) => pc.addTrack(track, streams))
+        streams.getTracks().forEach((track) => {
+          if (track.kind === 'video') {
+            const localVideo = document.querySelector('#local-video') as HTMLVideoElement
+            if (localVideo) localVideo.srcObject = streams
+          }
+          pc.addTrack(track, streams)
+        })
         const offer = await pc.createOffer()
         pc.setLocalDescription(offer)
 
@@ -102,12 +149,14 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
           caller: me,
           offer: offer
         }
+
         stompClient.subscribe(`/topic/calls/${localUUID}`, async (message: IMessage) => {
           switch (message.headers['x-call-event']) {
             case CallEvent.ANSWER:
               {
                 const data: Answer = JSON.parse(message.body)
                 console.log(`${data.callee.username} accepted the call!`)
+                remoteUUID = data.UUID
 
                 await pc.setRemoteDescription(new RTCSessionDescription(data.answer))
               }
@@ -115,19 +164,11 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
             case CallEvent.ICE_CANDIDATE:
               {
                 const data: RTCIceCandidateInit = JSON.parse(message.body)
-                console.log('Got remote ICE candidate', data.candidate)
                 pc.addIceCandidate(new RTCIceCandidate(data))
-                // localICECandidates.push(new RTCIceCandidate(data))
               }
               break
           }
         })
-        // const data = JSON.parse(message.body)
-        // if (data.type === CallEvent.ANSWER) {
-        //   pc.setRemoteDescription(new RTCSessionDescription(data.answer))
-        // } else if (data.type === CallEvent.ICE_CANDIDATE) {
-        //   pc.addIceCandidate(new RTCIceCandidate(data.candidate))
-        // }
         console.log('Sending invite', invite)
 
         stompClient.send(
@@ -153,14 +194,30 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
         pc.onconnectionstatechange = () => {
           console.log('[callee] RTC Connection state changed to:', pc.connectionState)
         }
-        pc.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
-          console.log('[callee] Got local ICE candidate', event.candidate)
-        }
         stompClient.subscribe(`/topic/calls/${localUUID}`, async (message: IMessage) => {
-          console.log('[callee] call signalling message', message.body)
+          switch (message.headers['x-call-event']) {
+            case CallEvent.ANSWER:
+              {
+                throw new Error('Unexpected ANSWER event')
+                const data: Answer = JSON.parse(message.body)
+                console.log(`${data.callee.username} accepted the call!`)
+
+                await pc.setRemoteDescription(new RTCSessionDescription(data.answer))
+              }
+              break
+            case CallEvent.ICE_CANDIDATE:
+              {
+                const data: RTCIceCandidateInit = JSON.parse(message.body)
+                console.log('Got remote ICE candidate', data.candidate)
+                pc.addIceCandidate(new RTCIceCandidate(data))
+                // localICECandidates.push(new RTCIceCandidate(data))
+              }
+              break
+          }
         })
 
         pc.onicecandidate = (event) => {
+          // immediately send as we're receiving the call, so the caller is already waiting for our ICE candidates
           if (event.candidate) {
             stompClient.send(
               `/topic/calls/${remoteUUID}`,
@@ -179,7 +236,13 @@ export async function useCall(me: User, stompClient: CompatClient): Promise<Call
         }
         pc.setRemoteDescription(new RTCSessionDescription(invite.offer))
         const streams = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-        streams.getTracks().forEach((track) => pc.addTrack(track, streams))
+        streams.getTracks().forEach((track) => {
+          if (track.kind === 'video') {
+            const localVideo = document.querySelector('#local-video') as HTMLVideoElement
+            if (localVideo) localVideo.srcObject = streams
+          }
+          pc.addTrack(track, streams)
+        })
         const sdp = await pc.createAnswer()
         pc.setLocalDescription(sdp)
         console.log(`Sending answer for ${remoteUUID}`, sdp)
--- src/views/Calls.vue
+++ src/views/Calls.vue
@@ -1,40 +1,58 @@
 <template>
   <div class="flex flex-col space-y-2 items-center">
-    <div class="text-center font-bold text-2xl text-white p-2 bg-blue-400 rounded">Logged in as: {{ auth?.user.username }}</div>
+    <div class="text-center font-bold text-2xl text-white p-2 bg-blue-400 rounded">
+      Logged in as: {{ auth?.user.username }}
+    </div>
     <div
       class="w-full"
-      @click="callAPI.call(user.username)"
+      @click="
+        () => {
+          callee = user
+          callAPI.call(user.username)
+        }
+      "
       v-for="user in users"
       :key="user.id"
     >
       <p class="p-2 bg-gray-300 rounded">
-        <fa-icon :icon="['fas', 'user']" class="mr-2 text-indigo-700"></fa-icon
-        >{{ user.username }}
+        <fa-icon :icon="['fas', 'user']" class="mr-2 text-indigo-700"></fa-icon>{{ user.username }}
       </p>
     </div>
     <div v-if="incomingCall" class="p-2 text-center">
       <p>Call from {{ incomingCall.caller.fullName || incomingCall.caller.username }}</p>
       <div class="flex flex-row space-x-2">
-        <button class="p-2 bg-green-400 text-white rounded" @click="accept">
-          Answer
-        </button>
+        <button class="p-2 bg-green-400 text-white rounded" @click="accept">Answer</button>
         <button class="p-2 bg-red-400 text-white rounded">Decline</button>
       </div>
     </div>
     <div class="w-full flex flex-row space-x-2">
-      <video
-        class="w-1/2 h-96 bg-gray-300 rounded-xl border-2 border-blue-500"
-        ref="localVideo"
-        id="local-video"
-        autoplay
-        muted
-      ></video>
-      <video
-        class="w-1/2 h-96 bg-gray-300 rounded-xl border-2 border-blue-500"
-        ref="remoteVideo"
-        id="remote-video"
-        autoplay
-      ></video>
+      <div class="relative">
+        <p
+          class="absolute bottom-0 w-full text-center text-2xl font-bold drop-shadow text-white rounded-b-xl bg-slate-500"
+        >
+          {{ auth.user.fullName }}
+        </p>
+        <video
+          class="h-96 bg-gray-300 rounded-xl border-2 border-blue-500"
+          ref="localVideo"
+          id="local-video"
+          autoplay
+          muted
+        ></video>
+      </div>
+      <div class="relative">
+        <p
+          class="absolute bottom-0 w-full text-center text-2xl font-bold drop-shadow text-white rounded-b-xl bg-slate-500"
+        >
+          {{ callee?.fullName || incomingCall?.caller.fullName || 'Waiting for call...' }}
+        </p>
+        <video
+          class="h-96 bg-gray-300 rounded-xl border-2 border-blue-500"
+          ref="remoteVideo"
+          id="remote-video"
+          autoplay
+        ></video>
+      </div>
     </div>
   </div>
 </template>
@@ -47,6 +65,7 @@ import type { Invite, CallAPI } from '@/composables/call'
 // Router ensures auth is defined
 const auth = unref(inject<Ref<Auth>>('auth'))!
 const users = ref<User[]>()
+const callee = ref<User | null>(null)
 
 const localVideo: Ref<HTMLVideoElement | null> = ref(null)
 const remoteVideo: Ref<HTMLVideoElement | null> = ref(null)
